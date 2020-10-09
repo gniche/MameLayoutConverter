39859b7644208826a098f4de62469533496037fa
ginog <ginogiuliani@hotmail.co.uk>
2020-10-09 02:03:43 +0100

    fixing layout for views with multiple layers by adding offset i.e. background>screen>bezel

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ folder = filepath + '\\' + currentGame + '\\'
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def processGameBezelFolder(currentFolder):
+def processLayoutFolder(currentFolder):
     mamelayout = eT.Element('mamelayout')
     mamelayout.set('version', '2')
 
@@ -28,9 +28,9 @@ def processGameBezelFolder(currentFolder):
             # item2.text = 'item2abc'
     xmlstr = minidom.parseString(eT.tostring(mamelayout)).toprettyxml(indent="   ")
     print(xmlstr)
-    # create a new XML file with the results
-    # myfile = open("items2.xml", "w")
-    # myfile.write(mydata)
+    with open(currentFolder + "default.lay", "w") as mame_layout_file:
+        mame_layout_file.write(xmlstr)
+        mame_layout_file.close()
 
 
 class Ini:
@@ -47,50 +47,73 @@ class Ini:
 
 def addView(mamelayout, currentFolder, bezel_name):
     ini = readIni(currentFolder, bezel_name)
-    bzImg = cv2.imread(currentFolder + bezel_name + '.png')
+    bz_img_shape = cv2.imread(currentFolder + bezel_name + '.png').shape
     addImageElement(mamelayout, bezel_name)
 
     view_elem = eT.SubElement(mamelayout, 'view')
     view_elem.set('name', bezel_name)
 
-    createScreenBoundsElem(view_elem, ini)
-    createBezelLayerElem(view_elem, bzImg, bezel_name)
+    bgimg = createbackgroundlayer(mamelayout, view_elem, bezel_name, currentFolder)
+
+    offset = getoffset(bgimg, bz_img_shape)
+    createScreenBoundsElem(view_elem, ini, offset)
+    createViewLayer(view_elem, bz_img_shape, bezel_name, offset)
+
+
+def getoffset(bgimg, bz_img_shape):
+    offset = (0, 0)
+    if bgimg is not None:
+        print("bgImage found!!")
+        offset = (bgimg.shape[0] / 2 - bz_img_shape[0] / 2, bgimg.shape[1] / 2 - bz_img_shape[1] / 2)
+    return offset
+
+
+def createbackgroundlayer(mamelayout, view_elem, bezel_name, currentFolder):
+    bgimg = None
     if bezel_name.startswith('Bezel - '):
-        try:
-            bgname = bezel_name.replace('Bezel - ', 'Background - ')
-            if os.path.exists(bgname + '.png'):
-                bgimg = cv2.imread(currentFolder + bgname + '.png')
-                addImageElement(mamelayout, bgname)
-                createBezelLayerElem(view_elem, bgimg, bgname)
-        except AttributeError as err:
-            print("No background image found:" + err)
+        bgname = bezel_name.replace('Bezel - ', 'Background - ')
+        bgPngPath = currentFolder + bgname + '.png'
+        bg_jpg_path = currentFolder + bgname + '.jpg'
+        if os.path.exists(bgPngPath):
+            bgimg = cv2.imread(bgPngPath)
+            addImageElement(mamelayout, bgname)
+            createViewLayer(view_elem, bgimg.shape, bgname)
+        elif os.path.exists(bg_jpg_path):
+            bgimg = cv2.imread(bg_jpg_path)
+            cv2.imwrite(bgPngPath, bgimg)
+            addImageElement(mamelayout, bgname)
+            createViewLayer(view_elem, bgimg.shape, bgname)
+    else:
+        print("simple config for: " + bezel_name)
+    return bgimg
 
 
-def addImageElement(mamelayout, bezelName):
-    elementElem = eT.SubElement(mamelayout, 'element')
-    elementElem.set('name', bezelName)
+def addImageElement(mamelayout, image_name):
+    elementElem = eT.Element('element')
+    mamelayout.insert(0, elementElem)
+    elementElem.set('name', image_name)
     imageElem = eT.SubElement(elementElem, 'image')
-    imageElem.set('file', bezelName + '.png')
+    imageElem.set('file', image_name + '.png')
 
 
-def createScreenBoundsElem(viewElem, ini):
+def createScreenBoundsElem(viewElem, ini, offset=(0, 0)):
     screenElem = eT.SubElement(viewElem, 'screen')
     screenElem.set('index', '0')  # single screen always 0
     sBoundsElem = eT.SubElement(screenElem, 'bounds')
-    sBoundsElem.set('x', str(ini.top_l_x))
-    sBoundsElem.set('y', str(ini.top_l_y))
+    sBoundsElem.set('x', str(offset[1] + ini.top_l_x))
+    sBoundsElem.set('y', str(offset[0] + ini.top_l_y))
     sBoundsElem.set('width', str(ini.btm_r_x - ini.top_l_x))
     sBoundsElem.set('height', str(ini.btm_r_y - ini.top_l_y))
 
 
-def createBezelLayerElem(viewElem, img, bezel_name):
-    bezelElem = eT.SubElement(viewElem, 'bezel')
-    bezelElem.set('element', bezel_name)
+def createViewLayer(viewElem, shape, image_name, offset=(0, 0)):
+    bezelElem = eT.SubElement(viewElem, 'element')
+    bezelElem.set('ref', image_name)
     bBoundsElem = eT.SubElement(bezelElem, 'bounds')
-    bBoundsElem.set('x', '0')
-    bBoundsElem.set('y', '0')
-    bBoundsElem.set('width', str(img.shape[0]))
-    bBoundsElem.set('height', str(img.shape[1]))
+    bBoundsElem.set('x', str(offset[1]))
+    bBoundsElem.set('y', str(offset[0]))
+    bBoundsElem.set('width', str(shape[1]))
+    bBoundsElem.set('height', str(shape[0]))
 
 
 def readIni(currentFolder, bezel_name):
@@ -113,4 +136,4 @@ def readIni(currentFolder, bezel_name):
     return ini
 
 
-processGameBezelFolder(folder)
+processLayoutFolder(folder)
